c33df2e805edae36ad62f2b015db5887d86f65f8
Valentina <Valentina_B@mail.ru>
2022-04-16 14:33:59 +0300

    Задача4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while(count <= numberA)
+{
+   if(count % 2 ==0)
+   {
+        Console.Write(count); 
+        if (count < numberA-1)  //Проверка, чтобы не выводилась последняя запятая
+        {
+        Console.Write(", "); 
+        }
+    }
+count++;
+}
+
