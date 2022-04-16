d2617d9665ba8f94329a0f387c692a73d287fc82
Valentina <Valentina_B@mail.ru>
2022-04-16 09:57:51 +0300

    Задача2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberС = Convert.ToInt32(Console.ReadLine());
+int numberMax = 0;
+
+if (numberA > numberB)
+{
+    numberMax = numberA;
+}
+else
+{
+    numberMax = numberB;
+}
+
+if (numberС > numberMax)
+{
+    numberMax = numberС;
+}
+
+Console.Write("Max=");
+Console.WriteLine(numberMax);
