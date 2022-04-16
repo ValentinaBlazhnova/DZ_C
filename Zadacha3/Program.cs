164374c3e8ab9e7bd6f6bd0d244bd68a4cbe2e8c
Valentina <Valentina_B@mail.ru>
2022-04-16 10:35:17 +0300

    Задача3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 ==0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
