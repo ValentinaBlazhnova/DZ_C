27b2394145b15dc283c3b21ab2b04b00eada9492
ValentinaBlazhnova <102962992+ValentinaBlazhnova@users.noreply.github.com>
2022-05-17 15:16:33 +0300

    Update Program.cs

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите первое число: ");
+/*Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Решение рабочее. Выводы 8+9 можно слепить в один и выводы 13+14 тоже.*/
+
+Console.WriteLine("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
